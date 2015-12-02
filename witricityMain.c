f22de1659335c99b5f4d95df9d6693ff8c7c1d78
BRIGHTER AGYEMANG <brighteragyemang@gmail.com>
2015-12-02 21:52:14 +0000

    Generation of k1k2k3

--- witricityMain.c
+++ witricityMain.c
@@ -28,23 +28,24 @@ typedef struct VerificationDetails
 
 typedef struct KeyExchangeDetails
 {
+    //received details
     element_t h_u;
     element_t upk_u;
     element_t R_u;
-    element_t randNum;
+    // a or b randomly selected from the group of the order of the curve cyclic group
+    element_t randKey;
+    //computed and sent/received details
     element_t T_b;
     element_t R_a;
 }KeyExchange;
 
-typedef struct ReceiverKeyExchangeDetails
+typedef struct SessionKeys
 {
-    element_t h_t;
-    element_t upk_t;
-    element_t R_t;
-    element_t a;
-    element_t T_b;
-    element_t R_a;
-}ReceiverKeyExchange;
+    element_t sU;//sRa or sTb
+    element_t k_1;
+    element_t k_2;
+    element_t k_3;
+}Keys;
 
 typedef struct PKGDetails
 {
@@ -63,20 +64,16 @@ typedef struct Entitys
     Verify verify_P;
     PrivateKey privateKey;
     PublicKey publicKey;
-    /**
-    *An entity can only have one of these members.
-    *Thus an entity can either be a transmitter or receiver
-    *at a point in time but cannot be both.
-    *These members are kept here to conveniently group the details
-    *an entity can have in the system
-    */
     KeyExchange keyExchange;
+    Keys sessionKeys;
 }Entity;
 
 void initPKG(PKG* pkg, char* systemParams);
 void initEntity(Entity* entity, PKG* pkg);
 void initVerificationDetails(Entity* entity, PKG* pkg);
 void initKeyExchangeDetails(Entity* entity, PKG* pkg);
+void computeSessionKeys(Entity* entity, PKG* pkg, element_t* el);
+unsigned long convertPointToLong(element_t* el);
 
 int main()
 {
@@ -221,9 +218,63 @@ printf("_____________________KEY GEN_____________________________________\n\n");
             //public key
             element_pow_zn(receiver.publicKey.upk_u, pkg.P, receiver.privateKey.usk_u);
             element_printf("Receiver's public key: %B\n\n", receiver.publicKey.upk_u);
-/*
-printf("_____________________KEY EXCHANGE_____________________________________\n\n");
-*/
+
+printf("_____________________KEY EXCHANGE_____________________________________\n");
+
+        //R sends details to T
+        element_set(transmitter.keyExchange.h_u, receiver.h_u);
+        element_set(transmitter.keyExchange.upk_u, receiver.publicKey.upk_u);
+        element_set(transmitter.keyExchange.R_u, receiver.privateKey.R_u);
+
+        //Initial Transmitter operations
+        element_random(transmitter.keyExchange.randKey);
+        element_printf("\nTransmitter's ephemeral random key b: %B\n",transmitter.keyExchange.randKey);
+        element_t inner_pow_t;
+        element_init(inner_pow_t, pkg.pairing->G1);
+        element_pow_zn(inner_pow_t, pkg.P_pub, transmitter.keyExchange.h_u);
+        element_t inner_addition_t;
+        element_init(inner_addition_t, pkg.pairing->G1);
+        element_add(inner_addition_t, transmitter.keyExchange.R_u, inner_pow_t);
+        element_pow_zn(transmitter.keyExchange.T_b, inner_addition_t, transmitter.keyExchange.randKey);
+        element_clear(inner_pow_t);
+        element_clear(inner_addition_t);
+        element_printf("\nT_b: %B\n", transmitter.keyExchange.T_b);
+
+        //T sends details to R
+        element_set(receiver.keyExchange.h_u, transmitter.h_u);
+        element_set(receiver.keyExchange.upk_u, transmitter.publicKey.upk_u);
+        element_set(receiver.keyExchange.R_u, transmitter.privateKey.R_u);
+        element_set(receiver.keyExchange.T_b, transmitter.keyExchange.T_b);//computed Tb of transmitter
+
+        //Initial Receiver operations
+        element_random(receiver.keyExchange.randKey);
+        element_printf("\nReceiver's ephemeral random key b: %B\n",receiver.keyExchange.randKey);
+        element_t inner_pow_r;
+        element_init(inner_pow_r, pkg.pairing->G1);
+        element_pow_zn(inner_pow_r, pkg.P_pub, receiver.keyExchange.h_u);
+        element_t inner_addition_r;
+        element_init(inner_addition_r, pkg.pairing->G1);
+        element_add(inner_addition_r, receiver.keyExchange.R_u, inner_pow_r);
+        element_pow_zn(receiver.keyExchange.R_a, inner_addition_r, receiver.keyExchange.randKey);
+        element_clear(inner_pow_r);
+        element_clear(inner_addition_r);
+        element_printf("\nR_a: %B\n", receiver.keyExchange.R_a);
+
+        //Receiver sends R_a to Transmitter
+        element_set(transmitter.keyExchange.R_a, receiver.keyExchange.R_a);
+
+printf("_____________________KEY COMPUTATION_____________________________________\n");
+
+        computeSessionKeys(&transmitter, &pkg, &transmitter.keyExchange.R_a);
+        element_printf("\nOutput debug T's k1:%B\n", transmitter.sessionKeys.k_1);
+        element_printf("\nOutput debug T's k2:%B\n", transmitter.sessionKeys.k_2);
+        element_printf("\nOutput debug T's k3:%B\n", transmitter.sessionKeys.k_3);
+
+        computeSessionKeys(&receiver, &pkg, &receiver.keyExchange.T_b);
+        element_printf("\nOutput debug R's k1:%B\n", receiver.sessionKeys.k_1);
+        element_printf("\nOutput debug R's k2:%B\n", receiver.sessionKeys.k_2);
+        element_printf("\nOutput debug R's k3:%B\n", receiver.sessionKeys.k_3);
+
 
     return 0;
 }
@@ -246,9 +297,16 @@ void initEntity(Entity* entity, PKG* pkg)
     element_init(entity->privateKey.s_u, pkg->pairing->Zr);
     element_init(entity->privateKey.usk_u, pkg->pairing->Zr);
     element_init(entity->publicKey.upk_u, pkg->pairing->G1);
+    element_init(entity->sessionKeys.sU, pkg->pairing->G1);
+    element_init(entity->sessionKeys.k_1, pkg->pairing->G1);
+    element_init(entity->sessionKeys.k_2, pkg->pairing->G1);
+    element_init(entity->sessionKeys.k_3, pkg->pairing->G1);
 
     //initialize entity's verification members
     initVerificationDetails(entity, pkg);
+
+    //initialize entity's key exchange members
+    initKeyExchangeDetails(entity, pkg);
 }
 
 void initVerificationDetails(Entity* entity, PKG* pkg)
@@ -260,5 +318,38 @@ void initVerificationDetails(Entity* entity, PKG* pkg)
 
 void initKeyExchangeDetails(Entity* entity, PKG* pkg)
 {
+    element_init(entity->keyExchange.h_u, pkg->pairing->Zr);
+    element_init(entity->keyExchange.upk_u, pkg->pairing->G1);
+    element_init(entity->keyExchange.R_u, pkg->pairing->G1);
+    element_init(entity->keyExchange.randKey, pkg->pairing->Zr);
+    element_init(entity->keyExchange.T_b, pkg->pairing->G1);
+    element_init(entity->keyExchange.R_a, pkg->pairing->G1);
+}
 
+void computeSessionKeys(Entity* entity,PKG* pkg, element_t* el)
+{
+    //s_tRa or s_tTb
+    element_pow_zn(entity->sessionKeys.sU, *el, entity->privateKey.s_u);
+    //K1
+    element_pow_zn(entity->sessionKeys.k_1, pkg->P, entity->keyExchange.randKey);
+    element_add(entity->sessionKeys.k_1, entity->sessionKeys.sU, entity->sessionKeys.k_1);
+    //K2
+    element_pow_zn(entity->sessionKeys.k_2, entity->sessionKeys.sU, entity->keyExchange.randKey);
+    //K3
+    element_t add1;
+    element_init(add1, pkg->pairing->G1);
+    element_pow_zn(add1, entity->keyExchange.upk_u, entity->keyExchange.randKey);
+    element_t add2;
+    element_init(add2, pkg->pairing->G1);
+    element_pow_zn(add2, entity->sessionKeys.sU, entity->privateKey.usk_u);
+    element_add(entity->sessionKeys.k_3, add1, add2);
+}
+
+unsigned long convertPointToLong(element_t* el)
+{
+    mpz_t s_mpz;
+    mpz_init(s_mpz);
+    element_to_mpz(s_mpz, *el);
+    element_printf("\nsdadss:%Z",s_mpz);
+    return mpz_get_ui(s_mpz);
 }
